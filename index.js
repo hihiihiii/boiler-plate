fb7a4607a10a5a7a34494e349352b8957b569e55
hihiihiii <ahrthfla@naver.com>
2022-11-07 01:15:30 +0900

    add

--- index.js
+++ index.js
@@ -2,19 +2,40 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-const adminPassword = encodeURIComponent(process.env.ADMIN_PASSWORD);
+const { User } = require("./model/Users");
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+//aplication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//aplication/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    `mongodb+srv://kim:824800@atlascluster.ct50okg.mongodb.net/?retryWrites=true&w=majority`
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("연결되었음"))
   .catch((err) => console.log(err, "에러남"));
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", (req, res) => {
-  res.send("hello world 안녕하세요 반갑습니다");
+  res.send("hello world 연결 연결완료");
+});
+
+app.post("/register", (req, res) => {
+  //회원가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    //만약 에러가 있다면 에러 메세지와 함께 보내고
+    if (err) return res.json({ success: false, err });
+    //아니면 성공했다라는 걸 보낸다.
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => console.log(`Exmple app listening on part ${port}`));
